838e9d170472aad064456fb450b7e46e4d5cd83d
Tomas Olivieri <Tomiolivieri.91@gmail.com>
2024-06-08 11:59:06 -0300

    update 3

--- project/Usuario/urls.py
+++ project/Usuario/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import home, lista_usuarios, create_usuario, acerca_de 
+from .views import home, lista_usuarios, create_usuario, acerca_de, usuario_delete, usuario_update, register, user_login, user_logout
+
 
 
 app_name = "Usuario"
@@ -8,5 +9,10 @@ urlpatterns = [
     path("", home, name="home"),
     path("lista", lista_usuarios, name="lista"),
     path("create", create_usuario, name="agregar_usuarios"),
-    path("acerca_de", acerca_de, name="acerca_de")
+    path("acerca_de", acerca_de, name="acerca_de"),
+    path("modificar/<int:pk>", usuario_update, name="usuario_update"),
+    path("borrar/<int:pk>", usuario_delete, name="usuario_delete"),
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 ]
--- project/Usuario/views.py
+++ project/Usuario/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Usuario
 from Usuario.forms import NuevoUsuarioForm
 
@@ -22,3 +25,47 @@ def create_usuario(request):
 
 def acerca_de(request):
     return render(request, "Usuario/acerca_de.html")
+
+def usuario_update(request,pk):
+    consulta = Usuario.objects.get(id=pk)
+    if request.method == "POST":
+        form = NuevoUsuarioForm(request.POST, instance=consulta)
+        if form.is_valid():
+            form.save()
+            return redirect("http://127.0.0.1:8000/Usuario/lista")
+    else:
+        form =  NuevoUsuarioForm(instance=consulta)
+    return render(request, "Usuario/usuario_update.html", {"form": form})
+
+def usuario_delete(request, pk):
+    consulta = get_object_or_404(Usuario, id=pk)
+    if request.method == "POST":
+        consulta.delete()
+        return redirect("http://127.0.0.1:8000/Usuario/lista")
+    return render(request, "Usuario/usuario_delete.html", {"object": consulta})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'Usuario/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('http://127.0.0.1:8000/') 
+    else:
+        form = AuthenticationForm()
+    return render(request, 'Usuario/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('http://127.0.0.1:8000/')
